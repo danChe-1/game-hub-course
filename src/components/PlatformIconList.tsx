2e3b7fd91d02cd9a79c589798b5069332f0aed2d
Daniel <dan.cherkasky@gmail.com>
2024-04-20 11:19:22 +0300

    filter games by genre

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -25,6 +25,7 @@ const PlatformIconList = ({ platforms }: Props) => {
     <HStack marginY={1}>
       {platforms.map((platform) => (
         <Icon
+          key={platform.id}
           color="gray.500"
           as={iconMap[platform.slug]}
         />
